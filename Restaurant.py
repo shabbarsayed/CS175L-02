c4193d35bab1c4bb06ac9c1503b480376efe5297
shabbarsayed <114018393+shabbarsayed@users.noreply.github.com>
2023-02-07 22:06:07 -0500

    Add files via upload

---
+++ Restaurant.py
@@ -0,0 +1,24 @@
+vegetarian = False
+vegan = False
+gluten_free = False
+
+response = input('Is anyone in your party a vegetarian? ')
+if response == 'yes':
+    vegetarian = True
+response = input('Is anyone in your party a vegan? ')
+if response == 'yes':
+    vegan = True
+response = input('Is anyone in your party gluten free? ')
+if response == 'yes':
+    gluten_free = True
+
+print('Here are you restaurant choices: ')
+if not vegetarian and not vegan and not gluten_free:
+    print("Joe's Gourmet Burgers")
+if not vegan and not gluten_free:
+    print("Mama's Fine Italian")
+if not vegan:
+    print("Main Street Pizza")
+
+print('Corner Cafe')
+print("Chef's Kitchen")
