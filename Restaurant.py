4058fc89a844aacc38677fd0c94fc4eda40d17ad
shabbarsayed <114018393+shabbarsayed@users.noreply.github.com>
2023-02-14 23:49:49 -0500

    Add files via upload

--- Restaurant.py
+++ Restaurant.py
@@ -1,3 +1,7 @@
+#Shabbar Sayed
+#CS 175L
+#restaurant.py
+
 vegetarian = False
 vegan = False
 gluten_free = False
@@ -22,3 +26,27 @@ if not vegan:
 
 print('Corner Cafe')
 print("Chef's Kitchen")
+
+keep_going = input("Enter 'yes' if you would like to do another restaurant search (enter 'no' to end): ")
+
+while keep_going =='yes':
+    response = input('Is anyone in your party a vegetarian? ')
+    if response == 'yes':
+        vegetarian = True
+    response = input('Is anyone in your party a vegan? ')
+    if response == 'yes':
+        vegan = True
+    response = input('Is anyone in your party gluten free? ')
+    if response == 'yes':
+        gluten_free = True
+
+        print('Here are you restaurant choices: ')
+        if not vegetarian and not vegan and not gluten_free:
+            print("Joe's Gourmet Burgers")
+        if not vegan and not gluten_free:
+            print("Mama's Fine Italian")
+            if not vegan:
+                print("Main Street Pizza")
+    print('Corner Cafe')
+    print("Chef's Kitchen")
+    keep_going = input("Enter 'yes' if you would like to do another restaurant search (enter 'no' to end): ")
