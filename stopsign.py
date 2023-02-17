ab8c22a36edac64a0a625f4fc170ac252a8cec01
shabbarsayed <114018393+shabbarsayed@users.noreply.github.com>
2023-02-16 22:40:04 -0500

    Add files via upload

---
+++ stopsign.py
@@ -0,0 +1,46 @@
+import math
+import turtle
+
+#CS175L-02
+#Shabbar Sayed
+#stopsign.py
+
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 400
+ANIMATION_SPEED = 0
+NUM_SIDES = 8
+LENGTH = 100
+ANGLE = 45
+TEXT_X = -5
+TEXT_Y = -10
+
+turtle.setup(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+s = LENGTH
+x = s/math.sqrt(2)
+diameter = s + (2 * x)
+
+screen = turtle.Screen()
+screen.bgcolor('black')
+
+t = turtle.Turtle()
+
+t.shape('turtle')
+
+t.penup()
+t.goto(-50, 100)
+t.pendown()
+t.color('white', 'red')
+t.begin_fill()
+for i in range(NUM_SIDES):
+    t.forward(LENGTH)
+    t.right(ANGLE)
+
+t.end_fill()
+t.hideturtle()
+
+t.penup()
+t.right(90)
+t.forward(140)
+t.color('white')
+t.write('STOP', font = ('Arial', 30, 'normal'))
